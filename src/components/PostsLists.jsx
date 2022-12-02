7378f27cbbe5ee22198f683145160d8734f08e81
Daniel <daniel-mcintyre@hotmail.com>
2022-12-02 13:56:49 -0500

    created PostsLists component and used useSelector to get the state from the slice

---
+++ src/components/PostsLists.jsx
@@ -0,0 +1,22 @@
+import { useSelect, useSelector } from 'react-redux';
+
+function PostsLists() {
+
+    const posts = useSelector(state => state.posts);
+
+    const renderedPosts = posts.map(post => (
+        <article key={posts.id}>
+            <h3>{posts.title}</h3>
+            <p>{posts.content.substring(0,100)}</p>
+        </article>
+    ))
+
+  return (
+    <section>
+        <h2>Posts</h2>
+        {renderedPosts}
+    </section>
+  )
+}
+
+export default PostsLists;
