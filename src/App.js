0284a559673dbaed45b7bff8b9e817df75b730a1
Daniel <daniel-mcintyre@hotmail.com>
2022-12-02 14:50:56 -0500

    created AddPostForm component and added state/functions to take in data from user

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import PostsLists from './components/PostsLists';
 import './App.css';
+import AddPostsForm from './components/AddPostsForm';
 
 function App() {
   return (
     <main className="App">
       <PostsLists/>
+      <AddPostsForm/>
     </main>
   );
 }
